c14ba3b66ebaee05007269c91edf8e10c6fb188d
Thomas Clavelli <thomasclavelli@DN51ud94.SUNet>
2017-10-26 18:51:13 -0700

    decomposed a little bit

--- server.js
+++ server.js
@@ -1,45 +1,51 @@
-'use strict';
-
 /*
  * A simple Node.js program for exporting the current working directory via a webserver listing
- * on a hard code (see portno below) port. To start the webserver run the command:
- *    node webServer.js
- *
- * Note that anyone able to connect to localhost:3001 will be able to fetch any file accessible
- * to the current user in the current directory or any of its children.
+ * on a hard code (see portno below) port
  */
 
-/* jshint node: true */
-
 var express = require('express');
-
 var portno = 3000;   // Port number to use
-
 var app = express();
-
-// We have the express static module (http://expressjs.com/en/starter/static-files.html) do all
-// the work for us.
 app.use(express.static(__dirname));
 
+
 app.get('/', function (request, response) {
-    response.send('Simple web server of files from ' + __dirname);
+  response.end('Simple web server of files from ' + __dirname);
 });
+
+
+function getBookDataForBookWithId(id) {
+  if(id % 2 === 0) {
+    return "Hello this is the text for book " + id
+  }
+}
+
+app.get('/book/:id', function(request, response) {
+  var id = request.params.id;
+  var bookText = getBookDataForBookWithId(id)
+  if(!bookText) {
+    response.status(404).end("Book with id " + id + " does not exist");
+    return;
+  }
+  response.end(JSON.stringify({book : bookText, id : id}));
+});
+
 
 var server = app.listen(portno, function () {
   var port = server.address().port;
   console.log('Listening at http://localhost:' + port + ' exporting the directory ' + __dirname);
 });
 
-var mysql = require('mysql');
+/*var mysql = require('mysql');*/
 
-var con = mysql.createConnection({
-  host: "localhost",
+/*var con = mysql.createConnection({
+  host: "10.37.165.93",
   user: "root",
   password: "webdevin",
   database: "CS50"
-});
+});*/
 
-con.connect(function(err) {
+/*con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
   con.query("select link from books where id=1", function (err, result) {
@@ -48,4 +54,4 @@ con.connect(function(err) {
    console.log("Result: " + JSON.stringify(result, null, 2));
    console.log(link);
  });
-});
+});*/
