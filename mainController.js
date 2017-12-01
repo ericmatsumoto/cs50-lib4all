ae885d402da6e71a59a6775af7f5f87cfb7eb3ce
“Eric <ematsu@stanford.edu>
2017-11-30 22:39:26 -0800

    added toggling between downloaded and full library views

--- mainController.js
+++ mainController.js
@@ -6,17 +6,25 @@ lib4AllApp.controller('MainController', ['$scope', '$resource',
 
         var downloadedBooksResource = $resource('/downloaded_books')
         $scope.downloadedBooks = downloadedBooksResource.query()
-        
+
 
         var resource = $resource("/books");
-        $scope.books = resource.query();
-        $scope.currentBooks = $scope.books;
+        $scope.allBooks = resource.query();
+        $scope.currentBooks = $scope.allBooks;
         $scope.filter = "";
         $scope.coverPrefix = "https://books.libraryforall.org/assets/";
+        var showingAllBooks = true;
+        $scope.buttonTitle = "My Downloaded Books";
 
         $scope.downloadBook = function(id) {
         	var downloadResource = $resource("/download/" + id);
         	var res = downloadResource.get();
+
+            var getBookResource = $resource("/books/" + id);
+            var book = getBookResource.get()
+            $scope.downloadedBooks.push(book);
+
+
         	console.log("Just downloaded book with id of " + id);
         }
 
@@ -33,6 +41,20 @@ lib4AllApp.controller('MainController', ['$scope', '$resource',
         		}
         		return false;
         	});
+        }
+
+        $scope.changeView = function() {
+            console.log("switched views supposedly")
+            if(showingAllBooks) {
+                $scope.currentBooks = $scope.downloadedBooks
+                showingAllBooks = false;
+                $scope.buttonTitle = "Full Catalog";
+            } else {
+                $scope.currentBooks = $scope.allBooks
+                showingAllBooks = true;
+                $scope.buttonTitle = "My Downloaded Books";
+            }
+
         }
 
     }]);
